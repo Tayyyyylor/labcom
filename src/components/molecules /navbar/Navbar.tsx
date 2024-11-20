ee46d6cdcb8df1d80915126b03a08527228d4d97
Tayyyyylor <bryan.h.pro@gmail.com>
2024-11-21 00:06:58 +0100

    struture du header

--- src/components/molecules /navbar/Navbar.tsx
+++ src/components/molecules /navbar/Navbar.tsx
@@ -1,5 +1,25 @@
+import LocalSwitcher from '@/components/atoms/localSwitcher/LocalSwitcher'
+import { useTranslations } from 'next-intl'
 import React from 'react'
+import styles from './Navbar.module.scss'
 
 export default function Navbar() {
-    return <div>Navbar</div>
+    const t = useTranslations()
+    return (
+        <nav className={styles.nav}>
+            <ul className={styles.navContainer}>
+                <li>{t('Header.nav1')}</li>
+                <li>{t('Header.nav2')}</li>
+                <li>{t('Header.nav3')}</li>
+                <li>{t('Header.nav4')}</li>
+                <hr className={styles.verticalBar} />
+                <li>
+                    <LocalSwitcher
+                        enOption={t('Global.language.en')}
+                        frOption={t('Global.language.fr')}
+                    />
+                </li>
+            </ul>
+        </nav>
+    )
 }

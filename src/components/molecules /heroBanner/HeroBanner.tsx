a17fecc3bcea447b73de507ce884e09eab45caef
Tayyyyylor <bryan.h.pro@gmail.com>
2024-11-21 18:37:50 +0100

    add fonts + header and img

--- src/components/molecules /heroBanner/HeroBanner.tsx
+++ src/components/molecules /heroBanner/HeroBanner.tsx
@@ -1,5 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
+import styles from './HeroBanner.module.scss'
 
 export default function HeroBanner() {
-    return <div>HeroBanner</div>
+    return (
+        <div className={styles.heroBanner}>
+            <Image
+                src="/01.png"
+                alt="Hero Banner"
+                fill
+                className={styles.imgBanner}
+            />
+        </div>
+    )
 }
--- src/components/templates/homepage/Homepage.tsx
+++ src/components/templates/homepage/Homepage.tsx
@@ -5,13 +5,44 @@ import styles from './Homepage.module.scss'
 import HeroBanner from '@/components/molecules /heroBanner/HeroBanner'
 import Title from '@/components/atoms/title/Title'
 import { Footer } from '@/components/layout/footer/Footer'
+import Image from 'next/image'
 
 export default function Homepage() {
     const t = useTranslations()
     return (
         <main className={styles.homepage}>
             <HeroBanner />
-            <Title title={t('Homepage.title')} />
+            <article>
+                <Title title={t('Homepage.title1')} />
+                <section>
+                    <Image src="" width={50} height={50} alt="art" />
+                    <div>
+                        <h3></h3>
+                        <p></p>
+                        <p></p>
+                        <Image src="" width={50} height={50} alt="art" />
+                    </div>
+                </section>
+            </article>
+
+            <article>
+                <Title title={t('Homepage.title2')} />
+                <section>
+                    <div>
+                        <Image src="" width={50} height={50} alt="art" />
+                        <div>text</div>
+                    </div>
+                    <div>
+                        <div>text</div>
+                        <Image src="" width={50} height={50} alt="art" />
+                    </div>
+                    <div>
+                        <Image src="" width={50} height={50} alt="art" />
+                        <div>text</div>
+                    </div>
+                </section>
+            </article>
+
             <Footer />
         </main>
     )

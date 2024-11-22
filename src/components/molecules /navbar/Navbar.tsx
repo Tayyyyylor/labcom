dbfcae064ae2368ed878ed20b4b34ba6da18c666
Tayyyyylor <bryan.h.pro@gmail.com>
2024-11-22 19:24:31 +0100

    add menu burger

--- src/components/molecules /navbar/Navbar.tsx
+++ src/components/molecules /navbar/Navbar.tsx
@@ -1,16 +1,68 @@
 'use client'
 import LocalSwitcher from '@/components/atoms/localSwitcher/LocalSwitcher'
 import { useTranslations } from 'next-intl'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Navbar.module.scss'
 import useMobile from '@/hooks/useMobile'
 
 export default function Navbar() {
     const t = useTranslations()
     const isMobile = useMobile()
-    console.log('isMobile', isMobile)
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    }
+
+    const handleClick = () => {
+        setIsOpen(false)
+    }
+
     return isMobile ? (
-        <nav>toto</nav>
+        <div className={styles.burgerMenu}>
+            <div className={styles.burgerIcon} onClick={toggleMenu}>
+                <span
+                    className={`${styles.line} ${isOpen ? styles.crossLine1 : ''}`}
+                />
+                <span
+                    className={`${styles.line} ${isOpen ? styles.crossLine2 : ''}`}
+                />
+                <span
+                    className={`${styles.line} ${isOpen ? styles.crossLine3 : ''}`}
+                />
+            </div>
+
+            <nav className={`${styles.menu} ${isOpen ? styles.menuOpen : ''}`}>
+                <ul>
+                    <li>
+                        <a href="#home" onClick={handleClick}>
+                            {t('Header.nav1')}
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#about" onClick={handleClick}>
+                            {t('Header.nav2')}
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#services" onClick={handleClick}>
+                            {t('Header.nav3')}
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contact" onClick={handleClick}>
+                            {t('Header.nav4')}
+                        </a>
+                    </li>
+                    <li>
+                        <LocalSwitcher
+                            enOption={t('Global.language.en')}
+                            frOption={t('Global.language.fr')}
+                        />
+                    </li>
+                </ul>
+            </nav>
+        </div>
     ) : (
         <nav className={styles.nav}>
             <ul className={styles.navContainer}>
